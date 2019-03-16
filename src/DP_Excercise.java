3d894cee78cc3350b9d5174def79621e0edcffdf
yongmin85 <yongmin85.cho@gmail.com>
2019-03-16 23:11:30 +0900

    commit

--- src/DP_Excercise.java
+++ src/DP_Excercise.java
@@ -2,7 +2,7 @@ public class DP_Excercise {
 
     public static void main(String[] args){
 
-        System.out.println("Hello World!!");
+
 
 
 
---
+++ src/DivideConquer_Excercise.java
@@ -0,0 +1,21 @@
+public class DivideConquer_Excercise {
+
+    public static void main(String[] arg){
+
+        for(int i = 1; i <= 100; i++){
+
+            System.out.println("FastSum[" + i + "] - " + fastSum(i));
+        }
+
+
+
+    }
+
+    public static int fastSum(int n){
+
+        if(n == 1) return n;
+        if(n%2 == 1) return fastSum(n-1) + n;
+
+        return  fastSum(n/2) * 2 + n*n/4;
+    }
+}
